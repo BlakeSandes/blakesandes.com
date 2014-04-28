5f1b509a897f5112442eda9ab7b77ce641ed1720
BlakeSandes <stellarshade@gmail.com>
2014-04-28 15:25:55 -0700

    Finally added some JQuery to my photo gallery by creating an overlay effect. Cool stuff

---
+++ js/main.js
@@ -0,0 +1,26 @@
+//JQuery for Gallery.
+
+//Create an overlay with a large image.
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+//Add image to overlay.
+$overlay.append($image);
+//Add caption to overlay.
+$overlay.append($caption);
+//Add overlay to body.
+$("body").append($overlay);
+
+//Capture the click event on a link to an image.
+$("#gallery a").click(function(event) {
+  event.preventDefault();
+  var imageLocation = $(this).attr("href");
+  $image.attr("src", imageLocation);
+  $overlay.show();
+})
+
+//When overlay is clicked.
+$overlay.click(function(){
+  //Hide overlay.
+  $overlay.hide();
+});
