345dbf94b20a4d37106568e77e1fdeafec693472
BlakeSandes <stellarshade@gmail.com>
2014-08-04 16:08:21 -0700

    Changed overlay image so that it opens relative to its containing li element instead of the body of the page. Took some fiddling, but I got it.

--- builds/development/js/script.js
+++ builds/development/js/script.js
@@ -108,29 +108,66 @@ $ = require('jquery');
 
 //////Create an overlay with a large image for the gallery.//////
 
-var $overlay = $('<div class="overlay"></div>');
-var $image = $("<img>");
-var $caption = $("<p></p>");
+
+// var $caption = $("<p></p>");
 //Add image to overlay.
-$overlay.append($image);
+// $overlay.append($image);
 //Add caption to overlay.
-$overlay.append($caption);
+// $overlay.append($caption);
 //Add overlay to body.
-$("body").append($overlay);
+// $("body").append($overlay);
 
 //Capture the click event on a link to an image.
-$(".gallery a:not([href^='http'])").click(function(event) {
+$(".gallery li:even" ).on( "click", "a:not([href^='http'])", function(event) {
   event.preventDefault();
-  var imageLocation = $(this).attr("href");
-  $image.attr("src", imageLocation);
-  $overlay.fadeIn();
-})
+  var $imageLocationEven = $(this).attr("href");
+  var $overlayImageEven = $('<img>');
+  var $overlayEven = $('<div class="overlayEven overlay"></div>');
+
+  $overlayImageEven.attr("src", $imageLocationEven);
+  $overlayEven.append($overlayImageEven);
+
+  //Append $overlayEven to its relative image in the gallery.
+  $(this).closest("li").append($overlayEven);
+
+  //Fade in the $overlayEven div.
+  $overlayEven.fadeIn(100);
+
+});
+
+$(".gallery li:odd").on("click", "a:not([href^='http'])", function(event) {
+  event.preventDefault();
+  var $imageLocationOdd = $(this).attr("href");
+  var $overlayImageOdd = $('<img>');
+  var $overlayOdd = $('<div class="overlayOdd overlay"></div>');
+
+
+  $overlayImageOdd.attr("src", $imageLocationOdd);
+  $overlayOdd.append($overlayImageOdd);
+
+   //Append $overlayOdd to its relative image in the gallery.
+  $(this).closest("li").append($overlayOdd);
+
+  //Fade in the $overlayOdd div.
+  $overlayOdd.fadeIn(100);
+
+});
+
+  //Get the image and assign to a variable.
+  
+  //Set the image in the $overlay div to be from the gallery image src.
+  
+
+  
+
+  
+  
 
 //When overlay is clicked.
-$overlay.click(function(){
+/*$("body:not('.overlay')").click(function(){
   //Hide overlay.
   $overlay.hide();
-});
+});*/
 
 
 
