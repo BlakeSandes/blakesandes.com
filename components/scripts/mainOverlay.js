82c94caf65ad12e171e49c79e69cb3993acccccd
BlakeSandes <stellarshade@gmail.com>
2014-08-04 21:35:13 -0700

    Changed nav margins. Playing around with script on nav to make overlay hide.

--- components/scripts/mainOverlay.js
+++ components/scripts/mainOverlay.js
@@ -4,25 +4,22 @@ $ = require('jquery');
 
 
 //////Create an overlay with a large image for the gallery.//////
+var $captionEven = $("<p>What's up Even!</p>");
 
 
-// var $caption = $("<p></p>");
-//Add image to overlay.
-// $overlay.append($image);
-//Add caption to overlay.
-// $overlay.append($caption);
-//Add overlay to body.
-// $("body").append($overlay);
+//Capture the click event on a link to an image in the even(left side) row.
 
-//Capture the click event on a link to an image.
 $(".gallery li:even" ).on( "click", "a:not([href^='http'])", function(event) {
   event.preventDefault();
   var $imageLocationEven = $(this).attr("href");
   var $overlayImageEven = $('<img>');
   var $overlayEven = $('<div class="overlayEven overlay"></div>');
+      $captionEven = $("<p>What's up Even!</p>");
 
+  //Set the image in the $overlay div to be from the gallery image src.
   $overlayImageEven.attr("src", $imageLocationEven);
   $overlayEven.append($overlayImageEven);
+  $overlayEven.append($captionEven);
 
   //Append $overlayEven to its relative image in the gallery.
   $(this).closest("li").append($overlayEven);
@@ -32,15 +29,20 @@ $(".gallery li:even" ).on( "click", "a:not([href^='http'])", function(event) {
 
 });
 
+
+//Capture the click event on a link to an image in the even(left side) row.
+
 $(".gallery li:odd").on("click", "a:not([href^='http'])", function(event) {
   event.preventDefault();
   var $imageLocationOdd = $(this).attr("href");
   var $overlayImageOdd = $('<img>');
   var $overlayOdd = $('<div class="overlayOdd overlay"></div>');
+  var $captionOdd = ("<p>What's up Odd!</p>");
 
-
+//Set the image in the $overlay div to be from the gallery image src.
   $overlayImageOdd.attr("src", $imageLocationOdd);
   $overlayOdd.append($overlayImageOdd);
+  $overlayOdd.append($captionOdd);
 
    //Append $overlayOdd to its relative image in the gallery.
   $(this).closest("li").append($overlayOdd);
@@ -50,21 +52,14 @@ $(".gallery li:odd").on("click", "a:not([href^='http'])", function(event) {
 
 });
 
-  //Get the image and assign to a variable.
-  
-  //Set the image in the $overlay div to be from the gallery image src.
-  
+   
+//Closing the overlay.
 
-  
-
-  
-  
-
-//When overlay is clicked.
-/*$("body:not('.overlay')").click(function(){
+$overlayOdd.click(function(){
   //Hide overlay.
-  $overlay.hide();
-});*/
+  $(this).hide();
+  console.log("Hey");
+});
 
 
 
