09257f5e77a966b5c7909f8f34a5b5dc981a1097
BlakeSandes <stellarshade@gmail.com>
2014-08-05 15:03:58 -0700

    Added previous next  and close buttons and styling for overlay. Close overlay on close button, button overlay not being removed from the dom??? Worked on reformatting code so overlay even and odd were the same function. Not yet successful.

--- components/scripts/mainOverlay.js
+++ components/scripts/mainOverlay.js
@@ -4,7 +4,9 @@ $ = require('jquery');
 
 
 //////Create an overlay with a large image for the gallery.//////
-var $captionEven = $("<p>What's up Even!</p>");
+
+var $overlayEven = $('<div class="overlayEven overlay"><span class="top"></span><span class="left"><</span><span class="right">></span><span class="close">X</span></div>');
+var $overlayOdd = $('<div class="overlayOdd overlay"><span class="top"></span><span class="left"><</span><span class="right">></span><span class="close">X</span></div>');
 
 
 //Capture the click event on a link to an image in the even(left side) row.
@@ -13,8 +15,8 @@ $(".gallery li:even" ).on( "click", "a:not([href^='http'])", function(event) {
   event.preventDefault();
   var $imageLocationEven = $(this).attr("href");
   var $overlayImageEven = $('<img>');
-  var $overlayEven = $('<div class="overlayEven overlay"></div>');
-      $captionEven = $("<p>What's up Even!</p>");
+  var $captionEven = $('<span class="caption"><p>Hey Even!</p></span>');
+
 
   //Set the image in the $overlay div to be from the gallery image src.
   $overlayImageEven.attr("src", $imageLocationEven);
@@ -36,15 +38,15 @@ $(".gallery li:odd").on("click", "a:not([href^='http'])", function(event) {
   event.preventDefault();
   var $imageLocationOdd = $(this).attr("href");
   var $overlayImageOdd = $('<img>');
-  var $overlayOdd = $('<div class="overlayOdd overlay"></div>');
-  var $captionOdd = ("<p>What's up Odd!</p>");
+  var $captionOdd = $('<span class="caption"><p>Hey Odd!</p></span>');
 
+  
 //Set the image in the $overlay div to be from the gallery image src.
   $overlayImageOdd.attr("src", $imageLocationOdd);
   $overlayOdd.append($overlayImageOdd);
   $overlayOdd.append($captionOdd);
 
-   //Append $overlayOdd to its relative image in the gallery.
+     //Append $overlayOdd to its relative image in the gallery.
   $(this).closest("li").append($overlayOdd);
 
   //Fade in the $overlayOdd div.
@@ -55,11 +57,16 @@ $(".gallery li:odd").on("click", "a:not([href^='http'])", function(event) {
    
 //Closing the overlay.
 
-$overlayOdd.click(function(){
+$overlayEven.on("click", ".close", function(){
   //Hide overlay.
-  $(this).hide();
-  console.log("Hey");
+  $(this).closest("div").fadeOut();
 });
+
+$overlayOdd.on("click", ".close", function(){
+  //Hide overlay.
+  $(this).closest("div").remove();
+});
+
 
 
 
